601f4e1bed0c2cd7c9721311f65a0d8a06479997
Victor <81167569+Victor-Manoel-Public@users.noreply.github.com>
2021-09-28 20:50:16 -0300

    Update DayOfBirth.java

--- Datas/com.exercicios/DayOfBirth.java
+++ Datas/com.exercicios/DayOfBirth.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-public class SimpleData {
+public class DayOfBirth {
     public static void main(String[] args) {
         Date date = new Date();
         SimpleDateFormat simpleDateFormat = null;
