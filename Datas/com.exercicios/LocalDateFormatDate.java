48b6ce859fae5b49e8edd4650e73e0f66db939c5
Victor <81167569+Victor-Manoel-Public@users.noreply.github.com>
2021-09-28 20:43:03 -0300

    Create LocalDateFormatDate.java

---
+++ Datas/com.exercicios/LocalDateFormatDate.java
@@ -0,0 +1,16 @@
+package com.exercicios;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DaysMonthsETC {
+    public static void main(String[] args) {
+        // datas formatadas com LocalDate
+        LocalDate lc = LocalDate.now();
+        System.out.println(lc.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        System.out.println("Dia da semana: " + lc.getDayOfWeek());
+        System.out.println("Dia do mês: " + lc.getDayOfMonth());
+        System.out.println("Mês: " + lc.getMonth());
+        System.out.println("Ano: " + lc.getYear());
+    }
+}
