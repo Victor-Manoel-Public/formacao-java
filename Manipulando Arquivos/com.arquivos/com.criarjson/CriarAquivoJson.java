ba7c24f660bcdaec67517abac6d913d1b80ae786
Victor <81167569+Victor-Manoel-Public@users.noreply.github.com>
2021-09-29 17:10:59 -0300

    Update CriarAquivoJson.java

--- Manipulando Arquivos/com.arquivos/com.criarjson/CriarAquivoJson.java
+++ Manipulando Arquivos/com.arquivos/com.criarjson/CriarAquivoJson.java
@@ -1,21 +1,25 @@
 package com.criarjson;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class CriarAquivoJson {
 
 	public static void main(String[] args) throws IOException {
-		
-    // listas com dados ficticios
+		// dados ficticios
 		DadosUsuarioJson usuario1 = new DadosUsuarioJson("Victor", "victor@email.com", "29/09/2021");
 		DadosUsuarioJson usuario2 = new DadosUsuarioJson("Manoel", "manoel@email.com", "29/09/2021");
 		DadosUsuarioJson usuario3 = new DadosUsuarioJson("Silvia", "silvia@email.com", "29/09/2021");
-		DadosUsuarioJson usuario4 = new DadosUsuarioJson("Ellen", "ellen@email.com", "29/09/2021");
+		DadosUsuarioJson usuario4 = new DadosUsuarioJson("Ellena", "ellena@email.com", "29/09/2021");
 
 		ArrayList<DadosUsuarioJson> usuarios = new ArrayList<>();
 		usuarios.add(usuario1);
@@ -24,7 +28,7 @@ public class CriarAquivoJson {
 		usuarios.add(usuario4);
 		
 
-		Gson gson = new GsonBuilder().setPrettyPrinting().create(); // colocar o json em formato organizado
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String json = gson.toJson(usuarios);
 		
 		
@@ -33,11 +37,27 @@ public class CriarAquivoJson {
 		if(!arquivo.exists()) {
 			arquivo.createNewFile();
 		}
-
+		// escrever os dados
 		FileWriter escreverNoArquivo = new FileWriter(arquivo);
-		escreverNoArquivo.write(json); // escrever json em arquivo
+		escreverNoArquivo.write(json);
 		escreverNoArquivo.flush();
 		escreverNoArquivo.close();
+		
+		// ler arquivo json
+		
+		FileReader jsonParaLer = new FileReader(arquivo);
+		JsonArray jsonArray = (JsonArray) JsonParser.parseReader(jsonParaLer);
+		
+		List<DadosUsuarioJson> listaDeUsuarios = new ArrayList<DadosUsuarioJson>();
+		
+		for (JsonElement elemento : jsonArray) {
+			DadosUsuarioJson usuario = new Gson().fromJson(elemento, DadosUsuarioJson.class);
+			listaDeUsuarios.add(usuario);
+		}
+		
+		for (DadosUsuarioJson dadosUsuarioJson : listaDeUsuarios) {
+			System.out.println(dadosUsuarioJson);
+		}
 	}
 
 }
